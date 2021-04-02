240f0abc085f64a573dffcc8809d260f3a9603bf
Adriano Castro <eaadrianosilvacastro@unipam.edu.br>
2021-04-02 00:01:45 -0300

    Atualizando pg login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,25 +5,27 @@ import '../styles/pages/login.css';
 
 
 function Login() {
- 
+
     return (
         <>
             <Header />
-           <form className='login1'>
-               <h3>Entrar</h3>
+            <div className="container">
+                <form className='login1'>
+                    <h3>Entrar</h3>
 
-                <hr/>
+                    <hr />
 
-               <label>Email</label>
-               <input type='email' name='email' placeholder=' adriano@gmail.com' />
+                    <label>Email</label>
+                    <input type='email' name='email' placeholder=' adriano@gmail.com' />
 
-               <label>Senha</label>
-               <input type='password' name='senha' placeholder=' **********'/>
-            <div className="esqSenha">
-               <Link to="/login">Esqueceu a senha?</Link>
+                    <label>Senha</label>
+                    <input type='password' name='senha' placeholder=' **********' />
+                    <div className="esqSenha">
+                        <Link to="/login">Esqueceu a senha?</Link>
+                    </div>
+                    <button type='submit' name='login'>Entrar</button>
+                </form>
             </div>
-               <button type='submit' name='login'>Entrar</button>
-           </form>
         </>
     )
 }
