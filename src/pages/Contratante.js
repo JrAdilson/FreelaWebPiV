39f77f1880e3f11cca156b3efa8f06320629b3df
Adriano Castro <eaadrianosilvacastro@unipam.edu.br>
2021-04-07 23:38:53 -0300

    Criando modal cad vaga

--- src/pages/Contratante.js
+++ src/pages/Contratante.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header/index';
 import '../styles/pages/contratante.css';
 import gandalf2 from '../assets/img/gandalf2.jpeg';
+import mais from '../assets/img/mais.png';
+import AddVaga from '../components/Modal/AddVaga';
+
 
 const PerfilContratante = () => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+
+
     return (
         <>
             <Header />
             <div className='principal'>
-                
+
             </div>
             <div className='conteudo'>
-                <img alt="profile" src={gandalf2} />
+                <div className='vagasCadastradas'>
+                    <h4>Vagas cadastradas</h4>
+                    <div className='mostrador'>
+
+                    </div>
+                </div>
+                <div className="profile">
+                    <img alt="profile" src={gandalf2} />
+                    <h3>Gandalf, o Cinzento</h3>
+                    <hr />
+                </div>
+                <div className='cadVaga'>
+                    <button onClick={() => setIsModalVisible(true)}><img src={mais}></img>Adicionar nova vaga</button>
+                    {isModalVisible ? (
+                        <AddVaga onClose={() => setIsModalVisible(false)}>
+
+                        </AddVaga>
+                    ) : null}
+
+                </div>
+
             </div>
         </>
     )
