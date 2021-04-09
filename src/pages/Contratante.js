afbe7b974e554f75e123970e1c8f5a7d5e8a6897
Adriano Castro <eaadrianosilvacastro@unipam.edu.br>
2021-04-08 21:46:28 -0300

    Perfil Contratante

--- src/pages/Contratante.js
+++ src/pages/Contratante.js
@@ -3,19 +3,23 @@ import Header from '../components/Header/index';
 import '../styles/pages/contratante.css';
 import gandalf2 from '../assets/img/gandalf2.jpeg';
 import mais from '../assets/img/mais.png';
+import foto from '../assets/img/foto.png';
 import AddVaga from '../components/Modal/AddVaga';
+
 
 
 const PerfilContratante = () => {
     const [isModalVisible, setIsModalVisible] = useState(false);
 
+    const [isCargo, setIsCargo] = useState("Mago");
+    const [isEmpresa, setIsEmpresa] = useState("Sociedade do Anel™");
 
 
     return (
         <>
             <Header />
             <div className='principal'>
-
+                <img id='foto' src={foto}></img>
             </div>
             <div className='conteudo'>
                 <div className='vagasCadastradas'>
@@ -29,16 +33,14 @@ const PerfilContratante = () => {
                     <h3>Gandalf, o Cinzento</h3>
                     <hr />
                 </div>
+                <p className='desc-emp'>{isCargo} na empresa {isEmpresa}</p>
                 <div className='cadVaga'>
                     <button onClick={() => setIsModalVisible(true)}><img src={mais}></img>Adicionar nova vaga</button>
                     {isModalVisible ? (
                         <AddVaga onClose={() => setIsModalVisible(false)}>
-
                         </AddVaga>
                     ) : null}
-
                 </div>
-
             </div>
         </>
     )
